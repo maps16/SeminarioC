18368a5aa0a22d6be5f05fe99a5d90296262b2f0
maps16 <mapsjr16@gmail.com>
2016-08-26 16:33:54 -0700

    Nada Nuevo

--- filtro.sh
+++ filtro.sh
@@ -6,9 +6,11 @@
 ## Login   <maparedes@ltsp146.example.com>
 ## 
 ## Started on  Fri Aug 26 12:45:49 2016 Martin Alejandro Paredes Sosa
-## Last update Time-stamp: <2016-ago-26.viernes 12:49:00 (maparedes)>
+## Last update Time-stamp: <2016-ago-26.viernes 16:28:29 (maparedes)>
 ##
 
 # Script para filtrar renglones de un archivo que contengan las cadenas de caracteres dados
 
-egrep -v 'PRES|hPa' allData.txt | egrep '76692|Show|LIFT|SWEAT|K|Totals|virtual|CAPV|Lifted|thickness|Precip' > DataFilter.csv
+egrep -v 'PRES|hPa' allData.txt | egrep '72274|Show|LIFT|SWEAT|K|Totals|virtual|CAPV|Lifted|thickness|Precip' > DataFilter.csv
+
+echo "OK"
