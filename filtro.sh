212ded977c09318c7b897130c0b7b03070017e53
maps16 <mapsjr16@gmail.com>
2016-09-09 11:57:28 -0700

    Algo

--- filtro.sh
+++ filtro.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ##
 ## filtro.sh
 ## 
@@ -6,11 +6,11 @@
 ## Login   <maparedes@ltsp146.example.com>
 ## 
 ## Started on  Fri Aug 26 12:45:49 2016 Martin Alejandro Paredes Sosa
-## Last update Time-stamp: <2016-ago-26.viernes 16:28:29 (maparedes)>
+## Last update Time-stamp: <2016-ago-30.martes 14:51:21 (maparedes)>
 ##
 
 # Script para filtrar renglones de un archivo que contengan las cadenas de caracteres dados
 
-egrep -v 'PRES|hPa' allData.txt | egrep '72274|Show|LIFT|SWEAT|K|Totals|virtual|CAPV|Lifted|thickness|Precip' > DataFilter.csv
+egrep -v 'PRES|hPa' allData.txt | egrep '72274|CAPE|K|SWEAT|Richardson|Showalter|Totals' > "##DataFilter1.csv##"
 
 echo "OK"
